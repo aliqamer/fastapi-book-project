1335b6993e75797da2d255bc6c6224ad4ad7f3f0
qamer ali <dev@investmentor.com.au>
2025-05-01 23:26:32 +0530

    hash pwd and store in db

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, Path
 from pydantic import BaseModel
+from database import SessionLocal
 from models import Users
+from starlette import status
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
 
 router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -12,8 +28,8 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str = "user"  # Default role is set to "user"
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     # Here you would typically hash the password and save the user to the database  
     # For now, we'll just return the user data as a response
     create_user_model = Users(
@@ -21,10 +37,12 @@ async def create_user(create_user_request: CreateUserRequest):
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
-        hashed_password=create_user_request.password,  # In a real app, hash the password
+        hashed_password=bcrypt_context.hash(create_user_request.password),  # In a real app, hash the password
         role=create_user_request.role,
         is_active=True  # Default to active
     )
     
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+    # db.refresh(create_user_model)
     
