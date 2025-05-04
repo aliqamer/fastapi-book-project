0fe1b09cd2e6a7f9dc5fb5215d434485962d8fbc
qamer ali <dev@investmentor.com.au>
2025-05-04 19:53:18 +0530

    postgreSQL db configurations

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 This project is to learn fastapi.
 
-To run the project 
+To run the project
+
 > Using uvicorn
-uvicorn books:app --reload
+> uvicorn books:app --reload
 
 > Fast api local
-fastapi dev .\app\main.py
-
+> fastapi dev .\app\main.py
 
 install fastapi and uvicorn
 
- > Pip install fastapi
+> Pip install fastapi
 
- > Pip install "uvicorn[standard]"
+> Pip install "uvicorn[standard]"
+
+install below packages for hashing passwords
+pip install passlib
+pip install bcrypt==4.0.1
+
+to submit form data in postman, install below package
+pip install python-multipart
+
+to work with JWT tokens, install below package
+pip install python-jose[cryptography]
+
+for postgreSQL dependency
+
+> pip install psycopg2-binary
--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos-app.db'
+# SQLAlchemy database URL
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos-app.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})  
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})  
+# PostgreSQL database URL
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5433/TodoApplicationDatabase'  # Replace with your PostgreSQL credentials
+engine = create_engine(SQLALCHEMY_DATABASE_URL)  
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
